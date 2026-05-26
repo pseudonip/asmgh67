26ecff0835f50e00fe76986ad844d5d0efa10677
cyteon <me@cyteon.dev>
2026-05-26 20:31:43 +0200

    rcode stats

--- apps/control/src/routes/admin/rcodeStats.tsx
+++ apps/control/src/routes/admin/rcodeStats.tsx
@@ -1,6 +1,13 @@
 import { ColumnDef } from "@tanstack/solid-table";
 import { createResource, createSignal } from "solid-js";
 import Table from "~/components/Table";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "~/components/ui/select";
 import { getRcodeRows } from "~/lib/server/admin.actions";
 import { Zone } from "~/lib/server/db/schema";
 
@@ -32,16 +39,46 @@ export default function AdminRcodeStats() {
 
   return (
     <main class="p-4 flex flex-col h-screen">
-      <div class="mb-4">
-        <h1 class="text-2xl ml-1 leading-none font-semibold">Rcode stats</h1>
-        <p class="text-sm text-muted-foreground ml-1 mt-1">
-          Latest stats based on dns rcodes
-        </p>
+      <div class="flex">
+        <div class="mb-4">
+          <h1 class="text-2xl ml-1 leading-none font-semibold">Rcode stats</h1>
+          <p class="text-sm text-muted-foreground ml-1 mt-1">
+            Latest stats based on dns rcodes
+          </p>
+        </div>
+
+        <div class="ml-auto w-32">
+          <Select
+            value={category()}
+            onChange={(value) => setCategory(value as any)}
+            options={[
+              "NOERROR",
+              "FORMERR",
+              "SERVFAIL",
+              "NXDOMAIN",
+              "NOTIMP",
+              "REFUSED"
+            ]}
+            class="mt-1"
+            itemComponent={(itemProps) => (
+              <SelectItem item={itemProps.item}>
+                {itemProps.item.rawValue}
+              </SelectItem>
+            )}
+          >
+            <SelectTrigger aria-label="Record type">
+              <SelectValue<string>>
+                {(state) => state.selectedOption()}
+              </SelectValue>
+            </SelectTrigger>
+            <SelectContent />
+          </Select>
+        </div>
       </div>
 
       <Table
         columns={columns}
-        data={[]}
+        data={rows()?.filter((r) => r.rcode === category()) || []}
         noEntriesMessage="No stats found"
       />
     </main>
