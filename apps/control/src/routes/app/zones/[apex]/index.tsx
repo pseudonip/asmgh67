36397d40d50e7a97f3a8beaca722692ea5640ee7
cyteon <me@cyteon.dev>
2026-05-27 19:52:49 +0200

    show status on overview

--- apps/control/src/routes/app/zones/[apex]/index.tsx
+++ apps/control/src/routes/app/zones/[apex]/index.tsx
@@ -1,6 +1,6 @@
 import { useNavigate } from "@solidjs/router";
 import { useZone } from "./context";
-import { createEffect } from "solid-js";
+import { createEffect, Show } from "solid-js";
 
 export default function Zone() {
   const navigate = useNavigate();
@@ -12,5 +12,28 @@ export default function Zone() {
     }
   });
 
-  return <main class="w-full h-screen flex px-2">zone overview</main>;
+  return (
+    <main class="p-4 px-5 flex flex-col h-screen">
+      <div>
+        <div class="flex">
+          <h1 class="text-2xl leading-none font-semibold">{zoneData()?.name}</h1>
+
+          <Show when={zoneData()?.status == "active"}>
+            <span class="text-sm text-ctp-green p-1 px-3 rounded-full bg-ctp-green/10 ml-auto">
+              Active
+            </span>
+          </Show>
+
+          <Show when={zoneData()?.status == "error"}>
+            <span class="text-sm text-ctp-red p-1 px-3 rounded-full bg-ctp-red/10 ml-auto">
+              Error
+            </span>
+          </Show>
+        </div>
+        <p class="text-sm text-muted-foreground mt-1">
+          Zone overview
+        </p>
+      </div>
+    </main>
+  );
 }
