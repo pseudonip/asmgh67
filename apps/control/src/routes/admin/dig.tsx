9d1f513f7f07229d98be400e4be425f4cad60c91
Cyteon <me@cyteon.dev>
2026-05-27 13:27:54 +0200

    idk

--- apps/control/src/routes/admin/dig.tsx
+++ apps/control/src/routes/admin/dig.tsx
@@ -1,8 +1,19 @@
 import { createSignal } from "solid-js"
+import { Button } from "~/components/ui/button";
+import {
+  TextField,
+  TextFieldInput,
+  TextFieldLabel,
+} from "~/components/ui/text-field";
+import { traceDig } from "~/lib/server/admin.actions";
 
 export default function AdminDig() {
   const [domain, setDomain] = createSignal<string>("");
   const [output, setOutput] = createSignal<string>("");
+
+  async function dig() {
+    setOutput(await traceDig(domain()));
+  }
 
   return (
     <main class="p-4 flex flex-col h-screen">
@@ -13,7 +24,16 @@ export default function AdminDig() {
         </p>
       </div>
 
-      <div class="rounded-lg border bg-card p-2 px-4 flex-1">
+      <div class="mb-4 rounded-lg bg-card border p-2 px-4 flex gap-4">
+        <TextField class="flex-1">
+          <TextFieldLabel>Domain</TextFieldLabel>
+          <TextFieldInput value={domain()} onInput={(e) => setDomain(e.target.value)} />
+        </TextField>
+
+        <Button onClick={dig} class="self-end w-32">Dig</Button>
+      </div>
+
+      <div class="rounded-lg border bg-card p-2 px-4 flex-1 whitespace-pre-wrap overflow-y-auto font-mono! text-sm">
         {output()}
       </div>
     </main>
